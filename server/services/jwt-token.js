20f63611a901fa16cb61ce5c1048dc1fc9a71c03
vkrantt <kr.vikrantkumar@gmail.com>
2024-03-02 19:20:50 +0530

    Fix env variable

--- server/services/jwt-token.js
+++ server/services/jwt-token.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.JWT_ENV !== "development",
+    secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 3 * 24 * 60 * 60 * 1000,
   });
