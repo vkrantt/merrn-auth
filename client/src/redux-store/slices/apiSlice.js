3f7a6a660a27f8ef644cc984df02e02e65a444c4
Vikrant Kumar <110630436+vkrantt@users.noreply.github.com>
2024-03-04 15:23:56 +0530

    added credentials in fetch api

--- client/src/redux-store/slices/apiSlice.js
+++ client/src/redux-store/slices/apiSlice.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // https://merrn-auth.onrender.com
 const baseQuery = fetchBaseQuery({
   baseUrl: "",
+  credential:"include"
 });
 
 export const apiSlice = createApi({
