a1b51b5b30722278312b611bcf7bc0906603f015
Vikrant Kumar <110630436+vkrantt@users.noreply.github.com>
2024-03-04 12:06:27 +0530

    change origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'https://merrn-auth.onrender.com'
+  origin: '*'
 }));
 
 // routes
